6e3e94d232c42a1d4644aaea778b9b08998190ae
Łukasz Piłatowski <lukpila29@gmail.com>
2022-01-18 19:02:23 +0100

    use python3.8 for twine upload

--- ci/upload-pypi.sh
+++ ci/upload-pypi.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-python3 -m pip install -U pip
-python3 -m pip install -U setuptools
-python3 -m pip install -U twine
+/opt/python/cp38-cp38/bin/python3.8 -m pip install -U pip
+/opt/python/cp38-cp38/bin/python3.8 -m pip install -U setuptools
+/opt/python/cp38-cp38/bin/python3.8 -m pip install -U twine
 
 # Uploads prebuilt binary distribution
 for file in wheelhouse/audited/*.whl; do
     echo "Uploading $file"
-    python3 -m twine upload --repository-url "$PYPI_SERVER" --username "$PYPI_USER" --password "$PYPI_PASSWORD" "$file"
+    /opt/python/cp38-cp38/bin/python3.8 -m twine upload --repository-url "$PYPI_SERVER" --username "$PYPI_USER" --password "$PYPI_PASSWORD" "$file"
 done
 
 # Uploads zip source distribution
 for file in wheelhouse/audited/*.zip; do
     echo "Uploading $file"
-    python3 -m twine upload --repository-url "$PYPI_SERVER" --username "$PYPI_USER" --password "$PYPI_PASSWORD" "$file"
+    /opt/python/cp38-cp38/bin/python3.8 -m twine upload --repository-url "$PYPI_SERVER" --username "$PYPI_USER" --password "$PYPI_PASSWORD" "$file"
 done
 
 # Uploads tar.gz source distribution
 for file in wheelhouse/audited/*.tar.gz; do
     echo "Uploading $file"
-    python3 -m twine upload --repository-url "$PYPI_SERVER" --username "$PYPI_USER" --password "$PYPI_PASSWORD" "$file"
+    /opt/python/cp38-cp38/bin/python3.8 -m twine upload --repository-url "$PYPI_SERVER" --username "$PYPI_USER" --password "$PYPI_PASSWORD" "$file"
 done
